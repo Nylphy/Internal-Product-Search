df3f086b996b9b560a6cb22a88ed5e9cb4360246
Montefusco <pedro.montefusco@outlook.com.br>
2023-05-21 19:12:55 -0300

    test

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,3 @@
 <h1 class="bg-red-500">Welcome to SvelteKit</h1>
-<button class="btn bg-primary hover:bg-secondary-focus"> Button </button>
+<button class="btn bg-secondary"> Button </button>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
